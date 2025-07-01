32d01a67ac3b19f55384953a814237b32c17177a
masam <compQx7@users.noreply.github.com>
2025-07-02 06:39:13 +0900

    feat(neovim): update filetype indent and tab settings

--- editor/nvim/lua/core/autocmds.lua
+++ editor/nvim/lua/core/autocmds.lua
@@ -14,13 +14,15 @@ end
 vim.api.nvim_create_augroup('FileTypeSpecificSettings', { clear = true })
 
 -- Define filetype-specific settings
+set_filetype_autocmds('astro', 2, true)
 set_filetype_autocmds('cs', 4, false)
-set_filetype_autocmds({ 'css', 'scss' }, 4, false)
+set_filetype_autocmds('css', 2, true)
+set_filetype_autocmds('scss', 4, false)
 set_filetype_autocmds('html', 2, true)
 set_filetype_autocmds({ 'javascript', 'javascriptreact' }, 4, false)
 set_filetype_autocmds('json', 2, true)
 set_filetype_autocmds('lua', 2, true)
 set_filetype_autocmds('markdown', 2, true)
-set_filetype_autocmds('rust', 4, false)
+set_filetype_autocmds('rust', 4, true)
 set_filetype_autocmds('sh', 4, true)
 set_filetype_autocmds({ 'typescript', 'typescriptreact' }, 2, false)
